adf43648ceee0149fd5f3a2020f2cefbc9533341
Robert BJ HC <bj112143@gmail.com>
2017-12-21 22:21:23 -0500

    snifer & form

--- 00-node-core/10-http-sniffer.js
+++ 00-node-core/10-http-sniffer.js
@@ -1,6 +1,40 @@
 'use strict';
 
+const http = require('http'),
+	options = {
+		host : 'escuela.digital',
+		port : 80,
+		path : '/'
+	};
+
+let htmlCode = '';
+
+function httpClient(res)
+{
+	console.log(`El sitio ${options.host} ha respondido. Código de Estado: ${res.statusCode}`);
+	res.on('data', data => {
+		htmlCode += data;
+		console.log(data, data.toString());
+	});
+}
+
 function httpError(err)
 {
-	console.log('');
+	console.log(`El sitio ${options.host} no ha respondido. Código de Estado: ${err.statusCode} Error: ${err.message}`);
 }
+
+function webServer(req, res) 
+{
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.end(htmlCode);
+}
+
+//instancia cliente de HTTP
+http
+	.get(options, httpClient)
+	.on('error', httpError);
+
+//instancia servidor de HTTP
+http
+	.createServer(webServer)
+	.listen( 3000, 'localhost', () => console.log('Servidor corriendo en http://localhost:3000/') );
